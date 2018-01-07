3df25d6ae9de03e6f18dbad0043a80b14fba103b
ddbrennan <ddbren@gmail.com>
2018-01-07 13:30:02 -0500

    scope variables declared

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'david';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob';
+}
+
+function attemptTwoFavoriteCustomers() {
+  const favoriteCustomer = 'david';
+  const favoriteCustomer = 'bob';
+}
