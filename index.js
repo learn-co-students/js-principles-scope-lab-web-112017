8bde7313a366ef11b32caaaa73c3d29f8c1218b1
KellyStamps <kellydsample@gmail.com>
2018-01-08 08:31:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Kelly';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not Kelly';
+}
+
+function attemptTwoFavoriteCustomers () {
+  let favoriteCustomer = 'Rigby';
+  let favoriteCustomer = 'Anthony';
+}
